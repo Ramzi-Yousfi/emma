25a4f5eafefd90b39bcb67eea064fff2285bd926
ramzi <ramzi446@hotmail.com>
2022-06-13 20:49:10 +0200

    hhhhfd

--- emma/urls.py
+++ emma/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from webapp import views
+from ..webapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', include('webapp.urls')),
 ]
