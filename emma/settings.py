13a30bc10b37f31712d67f3142ff6c5716bca6bc
ramzi <ramzi446@hotmail.com>
2022-10-12 11:33:44 +0200

    flex

--- emma/settings.py
+++ emma/settings.py
@@ -101,6 +101,7 @@ DATABASES = {
         'PASSWORD': 'a97e69de8c5e464af488eb7a2c8e4e4146dbf5413aea11045cd2ac7ac5f3f06b',
         'HOST': 'ec2-52-71-23-11.compute-1.amazonaws.com',
         'PORT': '5432',
+        
 }}"""
 
 DATABASES = {
